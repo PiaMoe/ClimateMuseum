5ccc4d0149164c30756b4c81a9a41672d7a01b2f
Theo1224 <matteo.knie@web.de>
2021-06-30 17:17:12 +0200

    update30.6
    einzelne Fälle für die Slider implementiert

--- Assets/Scripts/GlobusProjekt/Slider.cs
+++ Assets/Scripts/GlobusProjekt/Slider.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
+    public float temperature = 3.6f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,16 +15,48 @@ public class Slider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        Debug.Log(temperature);
     }
 
     public void Cow(float cownumber)
     {
-        Debug.Log(cownumber);
+        switch (cownumber) {
+
+            default:
+                Debug.Log("Keine Kühe");
+                break;
+            case 1:
+                // GameObject Kuh = this.gameObject.transform.GetChild(1).GetChild(0).gameObject;
+                // Kuh.SetActive(true);
+                // this.gameObject.transform.GetChild(1).GetChild(0).gameObject.SetActive(false);
+                Debug.Log("1 Kuh");
+                temperature += 1f;
+                break;
+            case 2:
+                Debug.Log("2 Kühe");
+                temperature += 2f;
+                break;
+        }
     }
 
     public void Plant(float plantnumber)
     {
-        Debug.Log(plantnumber);
+        switch (plantnumber)
+        {
+            default:
+                Debug.Log("Keine Pflanzen");
+                break;
+            case 1:
+                Debug.Log("1 Pflanze");
+                temperature -= 1f;
+                break;
+            case 2:
+                Debug.Log("2 Pflanzen");
+                temperature -= 2f;
+                break;
+        }
+
     }
+
+    
 }
